0bf5e536b5ce4876c5e75947ec791ac6c845ff5d
bloodearnest <simon.davy@thedatalab.org>
2023-04-17 16:27:20 +0100

    remove cefpython and use --app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
+import subprocess
+import threading
+import time
+
+import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
@@ -6,4 +12,28 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    html_content = """
+    <html>
+        <head>
+            <title>Test ACRO app</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+def run_chrome():
+    time.sleep(1)  # TODO: wait until port responds
+    # note: chrome.exe on windows
+    subprocess.run(["google-chrome", "--app=http://127.0.0.1:8000"], check=True)
+
+
+if __name__ == "__main__":
+    t = threading.Thread(target=run_chrome)
+    t.start()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    # TODO handle app exit from either thread
