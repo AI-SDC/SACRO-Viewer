610a0045ae8c1d392283ec130890db72c330d89e
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-05-30 12:41:45 +0100

    Add ESLint and Vite dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin", "external", "internal"],
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "never",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
