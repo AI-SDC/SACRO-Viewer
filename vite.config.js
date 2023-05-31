a96b8723155313879b830224b397fb8ae4880648
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-05-31 10:23:22 +0100

    Set up Vite with Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // ...
+  base: "/static/",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./assets/src/scripts/main.js",
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+    outDir: "assets/dist",
+    emptyOutDir: true,
+  },
 });
