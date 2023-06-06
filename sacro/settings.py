e74bddbeb09c88e2a0a58d51dba3210e350fecbd
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-06 13:53:42 +0100

    Add .env file and sample

--- sacro/settings.py
+++ sacro/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-olm!y%*+rm&)1x(j!y#q#941!@83y2p^!bs@7t_*w8z3gdrn6l"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
