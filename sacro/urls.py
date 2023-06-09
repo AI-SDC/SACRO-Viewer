d1fcea56991efdc6d1f6fcdbad6a3e8d92e83c5d
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-09 13:33:50 +0100

    Switch line endings to LF

--- sacro/urls.py
+++ sacro/urls.py
@@ -1,3 +1,6 @@
+import importlib.util
+
+from django.conf import settings
 from django.urls import include, path
 
 from sacro import views
@@ -5,5 +8,7 @@ from sacro import views
 
 urlpatterns = [
     path("", views.index),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG and importlib.util.find_spec("django_browser_reload"):
+    urlpatterns.append(path("__reload__/", include("django_browser_reload.urls")))
