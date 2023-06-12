0dd4beee65c4ff308555f9510930cbcecf21325a
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-12 14:23:34 +0100

    Set up Tailwind container sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ const tailwindForms = require("@tailwindcss/forms");
 module.exports = {
   content: ["sacro/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1rem",
+          lg: "2rem",
+          xl: "3rem",
+          "2xl": "3rem",
+        },
+      },
+    },
   },
   plugins: [tailwindForms],
 };
