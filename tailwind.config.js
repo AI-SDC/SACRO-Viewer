bb236e8b7e2fcb5813f0b7137ec65eafeb6c9c36
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-12 14:23:25 +0100

    Add Tailwind forms dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const tailwindForms = require("@tailwindcss/forms");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["sacro/templates/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindForms],
 };
