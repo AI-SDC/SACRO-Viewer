8853f4344b741362b3b10e8ffc0771fca6c59219
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-13 14:26:09 +0100

    Use signals to store state

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,17 +1,28 @@
+import { signal } from "@preact/signals";
 import "../styles/main.css";
 
-const filesData = new Map(
-  Object.entries(JSON.parse(document.getElementById("filesData").textContent))
-);
+const openFile = signal();
+
 const template = document.getElementById("fileRow");
 const container = document.getElementById("filesList");
 
-filesData.forEach((values, file) => {
-  const fileName = values.output.replace(/^.*[\\/]/, "");
+const handleFileClick = (e) => {
+  openFile.value = e.target.textContent;
+  document.getElementById("file-name-title").textContent = openFile.value;
+};
 
+const filesData = new Map(
+  Object.entries(
+    JSON.parse(document.getElementById("filesData").textContent).content_urls
+  )
+);
+
+filesData.forEach((url, fileName) => {
   const el = template.content.firstElementChild.cloneNode(true);
+
   el.querySelector(`[data-file="name"]`).textContent = fileName;
-  el.querySelector(`[data-file="link"]`).href = `#${file}`;
+  el.querySelector(`[data-file="link"]`).href = `#${url}`;
+  el.addEventListener("click", handleFileClick);
 
   container.appendChild(el);
 });
