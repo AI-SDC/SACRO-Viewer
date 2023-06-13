7e5bf6a1f3fdbdcc09c37a954191b64e70cffe4f
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-13 08:35:47 +0100

    Load file list via JS template

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,4 +1,17 @@
 import "../styles/main.css";
 
-// eslint-disable-next-line no-console
-console.log("Hello from the SACRO console!");
+const outputs = new Map(
+  Object.entries(JSON.parse(document.getElementById("OUTPUTS").textContent))
+);
+const template = document.getElementById("fileRow");
+const container = document.getElementById("filesList");
+
+outputs.forEach((values, file) => {
+  const fileName = values.output.replace(/^.*[\\/]/, "");
+
+  const el = template.content.firstElementChild.cloneNode(true);
+  el.querySelector(`[data-file="name"]`).textContent = fileName;
+  el.querySelector(`[data-file="link"]`).href = `#${file}`;
+
+  container.appendChild(el);
+});
