3adc94a23f6a48cf697b5c8c7371a42beb000f77
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-13 11:33:45 +0100

    Add more styled components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,24 @@
 const tailwindForms = require("@tailwindcss/forms");
 
+function addCssVarColors({ addBase, theme }) {
+  function extractColorVars(colorObj, colorGroup = "") {
+    return Object.keys(colorObj).reduce((vars, colorKey) => {
+      const value = colorObj[colorKey];
+
+      const newVars =
+        typeof value === "string"
+          ? { [`--color${colorGroup}-${colorKey}`]: value }
+          : extractColorVars(value, `-${colorKey}`);
+
+      return { ...vars, ...newVars };
+    }, {});
+  }
+
+  addBase({
+    ":root": extractColorVars(theme("colors")),
+  });
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["sacro/templates/**/*.html"],
@@ -17,5 +36,5 @@ module.exports = {
       },
     },
   },
-  plugins: [tailwindForms],
+  plugins: [tailwindForms, addCssVarColors],
 };
