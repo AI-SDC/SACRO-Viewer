d973e5f58244e42306eb6f3a34f752a49903d68b
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-06-20 13:33:45 +0100

    Improve file loading based on file type

--- assets/src/scripts/_file-loader.js
+++ assets/src/scripts/_file-loader.js
@@ -1,16 +1,28 @@
+import { canDisplay, isCsv, isImg } from "./_utils";
+
 async function fileLoader(openFile) {
   if (!openFile) return null;
   const response = await fetch(openFile.value.url);
 
   if (!response.ok) {
+    // eslint-disable-next-line no-console
     console.error(`An error has occurred: ${response.status}`);
     return null;
   }
 
   let res;
 
-  if (openFile.value.ext === "csv") {
-    res = await response.text();
+  if (canDisplay(openFile.value.url)) {
+    console.log(openFile.value.url);
+
+    if (isCsv(openFile.value.url)) {
+      res = await response.text();
+    }
+
+    if (isImg(openFile.value.url)) {
+      const blob = await response.blob();
+      res = URL.createObjectURL(blob);
+    }
   }
 
   return res;
