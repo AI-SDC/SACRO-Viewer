8d3de5b982bb95a1bd07cb06c28b581064070413
Lucy Bridges <lucy.bridges@thedatalab.org>
2023-06-28 11:52:38 +0100

    Add a Cypress e2e test
    This adds a "happy path" test, which successfully approves all files, releases
    them and checks that a zipfile is returned.
    
    It can be tested using npm run cypress:open and then clicking through to the
    spec file. This needs the server to be running in the background.
    
    There's a headless mode that can be used with npm run cypress:run too.

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,31 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:8000')
+    cy.get('#filesList li').each(($el, index, $list) => {
+      cy.wrap($el).click()
+      cy.get('button[name="approve"]').click()
+    })
+    // prepare for form submission that returns back a file
+    // https://on.cypress.io/intercept
+    cy.intercept({
+      pathname: '/review*',
+      method: 'POST',
+    }, (req) => {
+      // instead of redirecting to the Zip file
+      // and having the browser deal with it
+      // download the file ourselves
+      // but we cannot use Cypress commands inside the callback
+      // thus we will download it later using the captured URL
+      req.redirect('/')
+    }).as('records')
+
+    cy.get('button#openModalBtn').click()
+
+    cy.wait('@records').its('request').then((req) => {
+      cy.request(req)
+        .then(({ body, headers }) => {
+          expect(headers).to.have.property('content-type', 'application/x-zip-compressed')
+      })
+    })
+  })
+})
