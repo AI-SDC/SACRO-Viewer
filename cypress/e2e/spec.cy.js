a4f0391e52a4abe6e96c7a14bd3eaccf433ffdae
Lucy Bridges <lucy.bridges@thedatalab.org>
2023-06-29 13:50:55 +0100

    Improve targeting of elements
    The Cypress best practices recommend not typing the targeting of elements in
    tests to styling, JS event listeners or anything else that's subject to change.
    Instead, where possible it recommends the use of specific data attributes.
    
    See https://docs.cypress.io/guides/references/best-practices

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8000')
-    cy.get('#filesList li').each(($el, index, $list) => {
+    cy.get('[data-cy="filesList"] li').each(($el, index, $list) => {
       cy.wrap($el).click()
-      cy.get('button[name="approve"]').click()
+      cy.get('[data-cy="approve"]').click()
     })
     // prepare for form submission that returns back a file
     // https://on.cypress.io/intercept
@@ -11,11 +11,11 @@ describe('template spec', () => {
       pathname: '/review*',
       method: 'POST',
     }, (req) => {
-      // instead of redirecting to the Zip file
-      // and having the browser deal with it
-      // download the file ourselves
+      // instead of redirecting to the zip file
+      // and having the browser deal with it,
+      // we download the file ourselves
       // but we cannot use Cypress commands inside the callback
-      // thus we will download it later using the captured URL
+      // so we will download it later using the captured URL
       req.redirect('/')
     }).as('records')
 
