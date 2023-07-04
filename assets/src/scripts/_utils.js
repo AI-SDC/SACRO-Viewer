ec7250e2d42b08c5f49e15e1c312f43dccbf4aa2
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-07-04 15:37:12 +0100

    Add capitalisation, text and html helper functions

--- assets/src/scripts/_utils.js
+++ assets/src/scripts/_utils.js
@@ -11,3 +11,36 @@ export const isImg = (ext) =>
   ["gif", "jpg", "jpeg", "png", "svg"].includes(ext.toLowerCase());
 
 export const canDisplay = (ext) => isCsv(ext) || isImg(ext);
+
+/**
+ * Capitalise the first letter of a string
+ *
+ * @param {string} s - string to capitalise first letter
+ * @returns {string} - capitalised string
+ */
+export function capitalise(s) {
+  return (s && s[0].toUpperCase() + s.slice(1)) || "";
+}
+
+/**
+ * Set HTML to an element identified by the SACRO data attribute
+ *
+ * @param {string} element - data attribute name used in the template
+ * @param {string} code - HTML code to replace the innerHTML of the element
+ */
+export function setElementHTML(element, code) {
+  document.querySelector(
+    `[data-sacro-el="${element}"]`
+  ).innerHTML = html`${code}`;
+}
+
+/**
+ * Assign a text string to an HTML element identified by the SACRO
+ * data attribute
+ *
+ * @param {string} element - data attribute name used in the template
+ * @param {string} text - text to be replaced
+ */
+export function setElementText(element, text) {
+  document.querySelector(`[data-sacro-el="${element}"]`).innerText = text;
+}
