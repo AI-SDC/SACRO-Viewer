61f6ca37d9eed06af77c307c62079c759521b3c9
George Hickman <george@ghickman.co.uk>
2023-07-04 10:34:21 +0100

    Invoke a modal when clicking the Download and release button
    We're planning to do a lot more once all outputs are reviewed and the
    current thinking is to put that into a modal.  At least for the upcoming
    "global" comment.
    
    This adds the modal, as a component to keep the implementation details
    of that abstracted from the main template, invoked when the "Download
    and release" button is clicked.  I've split the functionality for
    enabling that button and submitting the form into two files now that
    they're conceptually different parts of the UI.

--- assets/src/scripts/_form-setup.js
+++ assets/src/scripts/_form-setup.js
@@ -1,47 +1,15 @@
-import { effect } from "@preact/signals";
-import { approvedFiles, fileComments, isReviewComplete } from "./_signals";
+import { approvedFiles, fileComments } from "./_signals";
 
 const formSetup = () => {
-  const form = document.querySelector("#approveForm");
-  const button = form.querySelector(`button[type="submit"]`);
+  const modal = document.querySelector("#submitModal");
 
-  const enabledClasses = [
-    "bg-blue-600",
-    "text-white",
-    "hover:bg-blue-700",
-    "focus:bg-blue-700",
-    "focus:ring-blue-500",
-    "focus:ring-offset-white",
-  ];
+  // close the modal
+  modal.querySelector(".cancel").addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
 
-  const disabledClasses = [
-    "cursor-not-allowed",
-    "bg-slate-300",
-    "text-slate-800",
-  ];
-
-  const setButtonState = (enabled) => {
-    if (enabled) {
-      if (button.disabled) {
-        button.classList.remove(...disabledClasses);
-        button.classList.add(...enabledClasses);
-        button.disabled = false;
-        button.setAttribute("title", "");
-      }
-    } else {
-      // disable
-      if (!button.disabled) button.classList.remove(...enabledClasses);
-      button.classList.add(...disabledClasses);
-      button.disabled = true;
-      button.setAttribute("title", "You must approve or reject all outputs");
-    }
-  };
-
-  setButtonState(false);
-
-  effect(() => setButtonState(isReviewComplete()));
-
-  form.addEventListener("formdata", (ev) => {
+  // update the form with approved files data before its submitted
+  document.querySelector("#approveForm").addEventListener("formdata", (ev) => {
     const data = Object.fromEntries(
       Object.keys(approvedFiles.value).map((output) => [
         output,
---
+++ assets/src/scripts/_modal-setup.js
@@ -0,0 +1,54 @@
+import { effect } from "@preact/signals";
+import { approvedFiles, isReviewComplete } from "./_signals";
+
+const modalSetup = () => {
+  const button = document.querySelector("#openModalBtn");
+  const modal = document.querySelector("#submitModal");
+
+  const enabledClasses = [
+    "bg-blue-600",
+    "text-white",
+    "hover:bg-blue-700",
+    "focus:bg-blue-700",
+    "focus:ring-blue-500",
+    "focus:ring-offset-white",
+  ];
+
+  const disabledClasses = [
+    "cursor-not-allowed",
+    "bg-slate-300",
+    "text-slate-800",
+  ];
+
+  const setButtonState = (enabled) => {
+    if (enabled) {
+      if (button.disabled) {
+        button.classList.remove(...disabledClasses);
+        button.classList.add(...enabledClasses);
+        button.disabled = false;
+        button.setAttribute("title", "");
+      }
+    } else {
+      // disable
+      if (!button.disabled) button.classList.remove(...enabledClasses);
+      button.classList.add(...disabledClasses);
+      button.disabled = true;
+      button.setAttribute("title", "You must approve or reject all outputs");
+    }
+  };
+
+  setButtonState(false);
+
+  effect(() => setButtonState(isReviewComplete()));
+
+  button.addEventListener("click", () => {
+    const approvedCount = Object.keys(approvedFiles.value).filter(
+      (k) => approvedFiles.value[k].approved
+    ).length;
+    modal.querySelector(".count").innerHTML = approvedCount;
+
+    modal.classList.remove("hidden");
+  });
+};
+
+export default modalSetup;
