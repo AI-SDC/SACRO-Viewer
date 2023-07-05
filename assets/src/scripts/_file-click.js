b0c6a1a609cd622fdb1b36f68c1762475043274c
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-07-05 12:52:48 +0100

    Refactor file-click

--- assets/src/scripts/_file-click.js
+++ assets/src/scripts/_file-click.js
@@ -1,3 +1,4 @@
+import { btnStyles, setButtonActive } from "./_buttons";
 import {
   createImageElement,
   createTableElement,
@@ -21,6 +22,66 @@ import {
   setElementText,
   toggleParentVisibility,
 } from "./_utils";
+
+/**
+ * Set style string for ACRO status
+ * @param {string} status - ACRO status with capitalised first letter
+ * @returns string
+ */
+function statusStyles(status) {
+  if (status === "Pass") return `bg-green-100 text-green-900`;
+  if (status === "Fail") return `bg-red-100 text-red-900`;
+  return `bg-yellow-100 text-yellow-900`;
+}
+
+/**
+ * Convert date string to standard en-GB format
+ * @param {Date} date - valid ISO date string
+ * @returns string
+ */
+function formatDate(date) {
+  const jsDate = new Date(date);
+
+  return jsDate.toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+}
+
+function checkComment(button, comment, openOutput) {
+  // if no comment, ensure button disabled. Other was ensure is enabled
+  if (comment.trim() === "") {
+    // ensure button is disabled
+    if (!button.disabled) {
+      button.disabled = true; // eslint-disable-line no-param-reassign
+      btnStyles.notDisabled
+        .split(" ")
+        .map((style) => button.classList.remove(style));
+      btnStyles.disabled.split(" ").map((style) => button.classList.add(style));
+      button.setAttribute("title", "You must enter a comment first");
+
+      approvedFiles.value = {
+        ...approvedFiles.value,
+        [openOutput]: {
+          approved: null,
+        },
+      };
+    }
+  } else if (button.disabled) {
+    // make sure button is enabled
+    button.disabled = false; // eslint-disable-line no-param-reassign
+    btnStyles.disabled
+      .split(" ")
+      .map((style) => button.classList.remove(style));
+    btnStyles.notDisabled
+      .split(" ")
+      .map((style) => button.classList.add(style));
+    button.setAttribute("title", "");
+  }
+}
 
 const fileClick = async ({ outputName, metadata, url }) => {
   // Set the file values
@@ -68,19 +129,10 @@ const fileClick = async ({ outputName, metadata, url }) => {
    */
   if (timestamp) {
     toggleParentVisibility("fileCreatedDate", "div", "show");
-
-    const createdAt = new Date(timestamp).toLocaleDateString("en-GB", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-    });
-
     setElementHTML(
       "fileCreatedDate",
       `<time datetime="${timestamp}" title="${timestamp}">
-      ${createdAt}
+      ${formatDate(timestamp)}
     </time>`
     );
   }
@@ -89,12 +141,6 @@ const fileClick = async ({ outputName, metadata, url }) => {
     toggleParentVisibility("fileType", "div", "show");
     setElementText("fileType", `${method ?? ""} ${type}`);
   }
-
-  const statusStyles = (status) => {
-    if (status === "Pass") return `bg-green-100 text-green-900`;
-    if (status === "Fail") return `bg-red-100 text-red-900`;
-    return `bg-yellow-100 text-yellow-900`;
-  };
 
   toggleParentVisibility("fileDetailsStatus", "div", "show");
 
@@ -156,18 +202,6 @@ const fileClick = async ({ outputName, metadata, url }) => {
 
   // Set the metadata
   const fileMetadata = document.getElementById("fileMetadata");
-
-  const btnStyles = {
-    default: `inline-flex items-center justify-center shadow-sm transition-buttons duration-200 px-4 py-2 text-sm font-medium`,
-    success: `bg-green-600 text-white border border-green-600 hover:bg-green-700 focus:bg-green-700 focus:ring-green-500 focus:ring-offset-white`,
-    successOutline: `bg-transparent border border-green-700 text-green-700 hover:bg-green-100 focus:bg-green-100 focus:ring-green-500 focus:ring-offset-white`,
-    // secondary: ``,
-    secondaryOutline: `bg-transparent border border-slate-400/75 text-slate-700 !shadow-none hover:bg-slate-200 focus:bg-slate-200 focus:ring-slate-500 focus:ring-offset-white`,
-    warning: `bg-red-600 text-white border border-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-white`,
-    warningOutline: `bg-transparent border border-red-700 text-red-700 hover:bg-red-100 focus:bg-red-100 focus:ring-red-500 focus:ring-offset-white`,
-    disabled: `cursor-not-allowed !bg-slate-300 !text-slate-800 !border-slate-400`,
-    notDisabled: `hover:shadow-lg focus:outline-none focus:ring-current`,
-  };
 
   fileMetadata.innerHTML = html`
     <div>
@@ -248,40 +282,6 @@ ${fileComments.value[openOutput]}</textarea
     `[data-sacro-el="fileDetailsTextareaComments"]`
   );
 
-  const checkComment = (button, comment) => {
-    // if no comment, ensure button disabled. Other was ensure is enabled
-    if (comment.trim() === "") {
-      // ensure button is disabled
-      if (!button.disabled) {
-        button.disabled = true; // eslint-disable-line no-param-reassign
-        btnStyles.notDisabled
-          .split(" ")
-          .map((style) => button.classList.remove(style));
-        btnStyles.disabled
-          .split(" ")
-          .map((style) => button.classList.add(style));
-        button.setAttribute("title", "You must enter a comment first");
-
-        approvedFiles.value = {
-          ...approvedFiles.value,
-          [openOutput]: {
-            approved: null,
-          },
-        };
-      }
-    } else if (button.disabled) {
-      // make sure button is enabled
-      button.disabled = false; // eslint-disable-line no-param-reassign
-      btnStyles.disabled
-        .split(" ")
-        .map((style) => button.classList.remove(style));
-      btnStyles.notDisabled
-        .split(" ")
-        .map((style) => button.classList.add(style));
-      button.setAttribute("title", "");
-    }
-  };
-
   const requireCommentButtons = [];
 
   if (metadata.status === "review") {
@@ -297,66 +297,31 @@ ${fileComments.value[openOutput]}</textarea
   }
 
   requireCommentButtons.forEach((button) =>
-    checkComment(button, commentInput.value)
+    checkComment(button, commentInput.value, openOutput)
   );
 
   approveButton.addEventListener("click", () => {
     setReviewState(openOutput, true);
-    btnStyles.successOutline
-      .split(" ")
-      .map((style) => approveButton.classList.remove(style));
-    btnStyles.success
-      .split(" ")
-      .map((style) => approveButton.classList.add(style));
-
-    btnStyles.warning
-      .split(" ")
-      .map((style) => rejectButton.classList.remove(style));
-    btnStyles.warningOutline
-      .split(" ")
-      .map((style) => rejectButton.classList.add(style));
+    setButtonActive(approveButton, "success", true);
+    setButtonActive(rejectButton, "warning", false);
   });
 
   resetButton.addEventListener("click", () => {
     setReviewState(openOutput, null);
-
-    btnStyles.success
-      .split(" ")
-      .map((style) => approveButton.classList.remove(style));
-    btnStyles.successOutline
-      .split(" ")
-      .map((style) => approveButton.classList.add(style));
-
-    btnStyles.warning
-      .split(" ")
-      .map((style) => rejectButton.classList.remove(style));
-    btnStyles.warningOutline
-      .split(" ")
-      .map((style) => rejectButton.classList.add(style));
+    setButtonActive(approveButton, "success", false);
+    setButtonActive(rejectButton, "warning", false);
   });
 
   rejectButton.addEventListener("click", () => {
     setReviewState(openOutput, false);
-
-    btnStyles.warningOutline
-      .split(" ")
-      .map((style) => rejectButton.classList.remove(style));
-    btnStyles.warning
-      .split(" ")
-      .map((style) => rejectButton.classList.add(style));
-
-    btnStyles.success
-      .split(" ")
-      .map((style) => approveButton.classList.remove(style));
-    btnStyles.successOutline
-      .split(" ")
-      .map((style) => approveButton.classList.add(style));
+    setButtonActive(rejectButton, "warning", true);
+    setButtonActive(approveButton, "success", false);
   });
 
   commentInput.addEventListener("keyup", () => {
     setComment(openOutput, commentInput.value);
     requireCommentButtons.forEach((button) =>
-      checkComment(button, commentInput.value)
+      checkComment(button, commentInput.value, openOutput)
     );
   });
 
