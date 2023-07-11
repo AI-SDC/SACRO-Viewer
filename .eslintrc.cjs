a41ff41b4806b7b8e9f3a691e576b8b26538fc51
Tom O'Dwyer <tom.odwyer@thedatalab.org>
2023-07-11 11:40:47 +0100

    Add ESLint plugin for JSDoc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "prettier", "plugin:cypress/recommended"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:cypress/recommended",
+    "plugin:jsdoc/recommended",
+  ],
   plugins: ["prettier", "cypress"],
   overrides: [
     {
