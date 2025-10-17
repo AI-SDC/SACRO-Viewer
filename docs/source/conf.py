77f2f5a6108d6579a0f566cbcec6c551faaada68
JessUWE <jessica.ikechukwu@uwe.ac.uk>
2025-10-17 12:18:43 +0100

    Included more content and worked on the userguide
    1. Included More content
    2. Includes more content in the introduction.rst, index.rst.
    3. Added more content on User guide
    4. Included a log.
    5. Developer guide

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
     "sphinx_issues",
     "sphinx_prompt",
     "pydata_sphinx_theme",
@@ -50,4 +51,23 @@ autodoc_default_options = {
     "special-members": "__init__",
     "undoc-members": True,
     "exclude-members": "__weakref__",
+}
+
+# Napoleon settings for Google/NumPy style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# Intersphinx mapping for external documentation
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "django": ("https://docs.djangoproject.com/en/stable/", "https://docs.djangoproject.com/en/stable/_objects/"),
 }
